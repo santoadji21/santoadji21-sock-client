e700962f77c61f8f3221d860c6c3335cc9c6a6af
santoadji21 <ahmad.aji.santoso21@gmail.com>
2022-03-24 10:45:25 +0700

    change socket url

--- src/Context/Socket/index.js
+++ src/Context/Socket/index.js
@@ -5,7 +5,7 @@ const SocketContext = React.createContext();
 
 const SocketProvider = ({ children }) => {
   // const ENDPOINT = 'http://localhost:5000/';
-  const ENDPOINT = 'https://santoadji21-sock-server.herokuapp.com:30986/';
+  const ENDPOINT = 'https://santoadji21-sock-server.herokuapp.com/';
   const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] });
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
